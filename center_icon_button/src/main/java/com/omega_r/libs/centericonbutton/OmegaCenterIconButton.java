45276cc55c9e0c46a9e3e612394b7d7ce6a6dd4a
roman_tcaregorodtcev <roman.tc@omega-r.com>
2018-03-21 13:02:54 +0300

    initial commit

---
+++ center_icon_button/src/main/java/com/omega_r/libs/centericonbutton/OmegaCenterIconButton.java
@@ -0,0 +1,173 @@
+package com.omega_r.libs.centericonbutton;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.Nullable;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.widget.AppCompatButton;
+import android.text.method.TransformationMethod;
+import android.util.AttributeSet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class OmegaCenterIconButton extends AppCompatButton {
+
+    private static final String DELIMITERS = "\n";
+    private static final int DRAWABLE_LEFT_POSITION = 0;
+    private static final int DRAWABLE_TOP_POSITION = 1;
+    private static final int DRAWABLE_RIGHT_POSITION = 2;
+    private static final int DRAWABLE_BOTTOM_POSITION = 3;
+    private static final int DRAWABLES_LENGTH = 4;
+
+    private Rect textBoundsRect;
+    @ColorInt
+    private int tintColor = Color.TRANSPARENT;
+
+    public OmegaCenterIconButton(Context context) {
+        super(context);
+        initDrawableTint(context, null);
+    }
+
+    public OmegaCenterIconButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initDrawableTint(context, attrs);
+    }
+
+    public OmegaCenterIconButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initDrawableTint(context, attrs);
+    }
+
+    private void initDrawableTint(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.OmegaCenterIconButton);
+            tintColor = typedArray.getColor(R.styleable.OmegaCenterIconButton_drawableTint, Color.TRANSPARENT);
+            float defaultDrawablePadding = getResources().getDimension(R.dimen.omega_default_drawable_padding);
+            int drawablePadding = (int) typedArray.getDimension(R.styleable.OmegaCenterIconButton_android_drawablePadding, defaultDrawablePadding);
+            setCompoundDrawablePadding(drawablePadding);
+
+            updateTint();
+            typedArray.recycle();
+        }
+    }
+
+    private void updateTint() {
+        if (tintColor != Color.TRANSPARENT) {
+            Drawable[] drawables = getCompoundDrawables();
+            for (Drawable drawable : drawables) {
+                if (drawable != null) {
+                    Drawable wrappedDrawable = DrawableCompat.wrap(drawable).mutate();
+                    DrawableCompat.setTint(wrappedDrawable, tintColor);
+                }
+            }
+            if (drawables.length == DRAWABLES_LENGTH) {
+                setCompoundDrawablesWithIntrinsicBounds(drawables[DRAWABLE_LEFT_POSITION],
+                                                        drawables[DRAWABLE_TOP_POSITION],
+                                                        drawables[DRAWABLE_RIGHT_POSITION],
+                                                        drawables[DRAWABLE_BOTTOM_POSITION]);
+            }
+        }
+    }
+
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(@DrawableRes int left, @DrawableRes int top, @DrawableRes int right, @DrawableRes int bottom) {
+        super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        updatePadding();
+    }
+
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(@Nullable Drawable left, @Nullable Drawable top, @Nullable Drawable right, @Nullable Drawable bottom) {
+        super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        updatePadding();
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        super.setText(text, type);
+        updatePadding();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        updatePadding(w);
+    }
+
+    private void updatePadding() {
+        updatePadding(getMeasuredWidth());
+    }
+
+    private void updatePadding(int width) {
+        if (width == 0) return;
+
+        Drawable[] compoundDrawables = getCompoundDrawables();
+        if (compoundDrawables.length == 0 || compoundDrawables.length != DRAWABLES_LENGTH) return;
+
+        Drawable leftDrawable = compoundDrawables[DRAWABLE_LEFT_POSITION];
+        Drawable rightDrawable = compoundDrawables[DRAWABLE_RIGHT_POSITION];
+        if (leftDrawable == null && rightDrawable == null) return;
+
+        int textWidth = getTextWidth();
+        int iconPadding = Math.max(getCompoundDrawablePadding(), 1);
+        int paddingSize;
+
+        if (leftDrawable != null && rightDrawable != null) {
+            paddingSize = (width - leftDrawable.getIntrinsicWidth() - rightDrawable.getIntrinsicWidth() - textWidth - iconPadding * 4) / 2;
+        } else if (leftDrawable != null) {
+            paddingSize = (width - leftDrawable.getIntrinsicWidth() - iconPadding * 2 - textWidth) / 2;
+        } else {
+            paddingSize = (width - rightDrawable.getIntrinsicWidth() - iconPadding * 2 - textWidth) / 2;
+        }
+
+        setPadding(paddingSize, getPaddingTop(), paddingSize, getPaddingBottom());
+    }
+
+    private int getTextWidth() {
+        if (textBoundsRect == null) {
+            textBoundsRect = new Rect();
+        }
+        Paint paint = getPaint();
+        String text = divideText();
+        paint.getTextBounds(text, 0, text.length(), textBoundsRect);
+        return textBoundsRect.width();
+    }
+
+    private String divideText() {
+        String text = getText().toString();
+        if (text.isEmpty()) {
+            return "";
+        }
+        List<String> list = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(text, DELIMITERS, false);
+        while (tokenizer.hasMoreTokens()) {
+            list.add(tokenizer.nextToken());
+        }
+        if (list.size() == 1) {
+            return isAllCaps() ? list.get(0).toUpperCase() : list.get(0);
+        }
+        String longPart = list.get(0);
+        for(int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i + 1).length() > list.get(i).length()) {
+                longPart = list.get(i + 1);
+            }
+        }
+
+        return isAllCaps() ? longPart.toUpperCase() : longPart;
+    }
+
+    private boolean isAllCaps() {
+        TransformationMethod method = getTransformationMethod();
+        if(method == null) return false;
+
+        return method.getClass().getSimpleName().equals("AllCapsTransformationMethod");
+    }
+
+}
